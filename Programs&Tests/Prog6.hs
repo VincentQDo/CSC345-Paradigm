9b5c401951b4f394c0155b4d204be2e5b23ea768
Vincent <QD899836@wcupa.edu>
2019-04-13 13:04:14 -0400

    accept the Changes

--- Programs&Tests/Prog6.hs
+++ Programs&Tests/Prog6.hs
@@ -10,36 +10,16 @@ data Tree1 = Leaf1 Int
 --1. Write a function preorder that takes a tree argument and returns as a list an inorder traversal of the tree.
 preorder :: Tree1 -> [Int]
 preorder (Leaf1 n) = [n]
-<<<<<<< HEAD
 preorder (Node1 left r right) = [r] ++ preorder left ++ preorder right
-=======
-preorder tree1 =  case tree1 of
-         Node1 (Leaf1 l1) n1 (Leaf1 l2) -> [n1,l1,l2]
-         Node1 n2 root (Leaf1 l2) -> [root] ++ preorder n2 ++ [l2]
-         Node1  (Leaf1 l2) root n2 -> [root]  ++ [l2] ++ preorder (n2)
-         Node1 n2 root n3 -> [root]  ++ preorder n2 ++ preorder n3
->>>>>>> master
 
 --2. Write a function postorder that takes a tree argument and returns as a list an inorder traversal of the tree.
 postorder :: Tree1 -> [Int]
 postorder (Leaf1 n) = [n]
-<<<<<<< HEAD
 postorder (Node1 n2 root n3) =  postorder n2 ++ postorder n3 ++ [root]
 
 --3. Write a function sumPositives that takes a tree argument and returns the sum of positive integers in the tree.
 sumpositives :: Tree1 -> Int
 sumpositives tree = sum [x | x <- postorder tree]
-=======
-postorder tree1 =  case tree1 of
-         Node1 (Leaf1 l1) n1 (Leaf1 l2) -> [l1,l2,n1]
-         Node1 n2 root (Leaf1 l2) -> postorder n2 ++ [l2] ++ [root]
-         Node1  (Leaf1 l2) root n2 -> [l2] ++ postorder n2 ++ [root]
-         Node1 n2 root n3 -> postorder n2 ++ postorder n3 ++ [root]
-
---3. Write a function sumPositives that takes a tree argument and returns the sum of positive integers in the tree.
-sumpositives :: Tree1 -> Int
-sumpositives tree = sum [x | x <- postorder tree, x > 0]
->>>>>>> master
 
 -- 4. Write a function countInteriorNodes that returns the number of interior nodes in the given tree.
 countInteriorNodes :: Tree1 -> Int
