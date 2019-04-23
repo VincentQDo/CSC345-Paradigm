c68634b337ea6dbccfa86b522daa40b5855755b0
Vincent <vincentquangdo@gmail.com>
2019-04-23 12:08:17 -0400

    half way on HW8

--- Programs&Tests/Prog8.hs
+++ Programs&Tests/Prog8.hs
@@ -3,17 +3,35 @@
 		Vincent Do, May 2019
 ######################################################## -}
 module Prog8 where
-{-
+
 --1. Write a function sumSqNeg that computes the "sum of squares of negatives". You must use one or more higher-order functions: map, filter, foldr
 sumSqNeg :: [Int] -> Int
+sumSqNeg xs = sum (map doubleNegNum (filter isNeg xs))
+
+doubleNegNum :: Int -> Int
+doubleNegNum n = -(n * n)
+
+isNeg :: Int -> Bool
+isNeg n = n < 0
 --2. Write a function containing (without any higher order functions) that returns whether each element in the first list is also in the second list.
 containing :: Eq a => [a] -> [a] -> Bool
+containing [] [] = True
+containing [] ys = True
+containing xs [] = False
+containing (x:xs) ys = case elem x ys of
+    True -> containing xs ys
+    False -> False
 --3. Write a function total that applies the function (first argument) to every element in the list (second argument) and sums the result. You must use one or more higher-order functions: map, filter, foldr
 total :: (Int -> Int) -> [Int] -> Int
+total fn xs = sum (map fn xs)
 --4. Write a function containing' (with higher order functions) that returns whether each element in the first list is also in the second list. You must use one or more higher-order functions: map, filter, foldr
 containing' :: Eq a => [a] -> [a] -> Bool
+containing' xs ys = length (filter ) == length xs
 --5. Write a function lengths that returns a list of lengths of the given strings. You must use one or more higher-order functions: map, filter, foldr.
 lengths :: [String] -> [Int]
+lengths xs = map length xs
+{-
+
 --6. Write a function product' that returns the product of a nonempty list of numbers. You must use one or more higher-order functions: map, filter, foldr.
 product' :: Num a => [a] -> a
 --7. Write a function max' that returns the largest element of a nonempty list. You must use one or more higher-order functions: map, filter, foldr.
--- Programs&Tests/Test8.hs
+++ Programs&Tests/Test8.hs
@@ -27,11 +27,53 @@ First time user type in the command below
    tests :: TestTree
    tests = testGroup "Tests" [unitTests]
    
+   intList :: [Int]
+   intList = [(-1),(-2),(-3),4]
+   intList2 :: [Int]
+   intList2 = []
+   intList3 :: [Int]
+   intList3 = [0,2,3,1,4]
+   intList4 :: [Int]
+   intList4 = [0,(-1)]
+   intList5 :: [Int]
+   intList5 = [(-1),(-2),(-3),4,5,6]
    
+   stringList :: [String]
+   stringList = ["hello", "hi", "long", ""]
    unitTests = testGroup "Unit tests"
      [
    
-         testCase "test1a" $ assertEqual [] [] (),
+         testCase "test1a" $ assertEqual [] (-14) (sumSqNeg intList),
+         testCase "test1b" $ assertEqual [] 0 (sumSqNeg intList2),
+         testCase "test1c" $ assertEqual [] 0 (sumSqNeg intList3),
+         testCase "test1d" $ assertEqual [] (-1) (sumSqNeg intList4),
+
+         testCase "test2a" $ assertEqual [] True (containing intList intList5),
+         testCase "test2b" $ assertEqual [] False (containing intList5 intList),
+         testCase "test2c" $ assertEqual [] True (containing intList2 intList5),
+         testCase "test2d" $ assertEqual [] True (containing intList2 intList2),
+         testCase "test2e" $ assertEqual [] False (containing intList5 intList2),
+
+         testCase "test3a" $ assertEqual [] (-30) (total doubleNegNum intList),
+         testCase "test3b" $ assertEqual [] 0 (total doubleNegNum intList2)
+
+         testCase "test4a" $ assertEqual [] True (containing' intList intList5),
+         testCase "test4b" $ assertEqual [] False (containing' intList5 intList),
+         testCase "test4c" $ assertEqual [] True (containing' intList2 intList5),
+         testCase "test4d" $ assertEqual [] True (containing' intList2 intList2),
+         testCase "test4e" $ assertEqual [] False (containing' intList5 intList2),
+
+         testCase "test5a" $ assertEqual [] [5,2,4,0] (lengths stringList),
+         testCase "test5b" $ assertEqual [] [] (lengths [])
+
+
+
+
+
+
+
+
+
 
      ]
    
