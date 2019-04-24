9dd4c881513e75ef6bf99146f37d5be28f25b969
Vincent <vincentquangdo@gmail.com>
2019-04-24 13:31:48 -0400

    Finished HW8 with minor changes

--- Programs&Tests/Prog8.hs
+++ Programs&Tests/Prog8.hs
@@ -10,6 +10,7 @@ sumSqNeg xs = foldr (+) 0 (map doubleNegNum (filter isNeg xs))
   where 
     doubleNegNum x = x * x
     isNeg x = x < 0
+
 --2. Write a function containing (without any higher order functions) that returns whether each element in the first list is also in the second list.
 containing :: Eq a => [a] -> [a] -> Bool
 containing [] [] = True
@@ -18,31 +19,36 @@ containing xs [] = False
 containing (x:xs) ys = case elem x ys of
     True -> containing xs ys
     False -> False
+
 --3. Write a function total that applies the function (first argument) to every element in the list (second argument) and sums the result. You must use one or more higher-order functions: map, filter, foldr
 total :: (Int -> Int) -> [Int] -> Int
 total fn xs = sum (map fn xs)
+
 --4. Write a function containing' (with higher order functions) that returns whether each element in the first list is also in the second list. You must use one or more higher-order functions: map, filter, foldr
 containing' :: Eq a => [a] -> [a] -> Bool
 containing' xs ys = and (map (containingHelp ys) xs) --this function will execute containingHelp on every item in xs and check if it belongs in ys
-
 --this function helper is to check if an item is an element of a list. This function will take in one single list then return a function
 containingHelp :: Eq a => [a] -> (a -> Bool)
-containingHelp xs = fn --the retunred function then take in an item then check if that one item is in the list that was taken in by helper function
+containingHelp xs = fn --This function will take in a list then return a function, that function will take in a single literal then check if that literal is an element of the list from the containingHelp function
   where
     fn x = elem x xs
---TODO:: need to finish this
+
 --5. Write a function lengths that returns a list of lengths of the given strings. You must use one or more higher-order functions: map, filter, foldr.
 lengths :: [String] -> [Int]
 lengths xs = map length xs
+
 --6. Write a function product' that returns the product of a nonempty list of numbers. You must use one or more higher-order functions: map, filter, foldr.
 product' :: Num a => [a] -> a
 product' xs = foldr (*) 1 xs
+
 -- --7. Write a function max' that returns the largest element of a nonempty list. You must use one or more higher-order functions: map, filter, foldr.
 max' :: (Ord a, Num a) => [a] -> a
 max' xs = foldr max 0 xs
+
 --8. Write a function append' that appends two lists. You must use one or more higher-order functions: map, filter, foldr.
 append' :: [a] -> [a] -> [a]
 append' xs ys = foldr (:) ys xs 
+
 --9. Write a function filterFirst that removes the first element from the list (second argument) that does not satisfy a given predicate function (first argument). You must use one or more higher-order functions: map, filter, foldr.
 filterFirst :: Eq a => (a -> Bool) -> [a] -> [a]
 filterFirst fn xs = case filter fn xs of 
@@ -53,6 +59,7 @@ filterFirst fn xs = case filter fn xs of
         removeFirst x (y:ys) = case x == y of
             True -> ys
             False -> y : removeFirst x ys
+
 --10. Write a function filterLast that removes the last element from the list (second argument) that does not satisfy a given predicate function (first argument). You must use one or more higher-order functions: map, filter, foldr.
 filterLast :: Eq a => (a -> Bool) -> [a] -> [a]
 filterLast fn xs = reverse (filterFirst fn (reverse xs))
